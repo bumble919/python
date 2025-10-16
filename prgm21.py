e220f0dc3592b8c0c9f942338e4dc6119dda823f
shanu3785 <bumblebe919@gmail.com>
2025-10-16 10:33:54 +0530

    Add files via upload

---
+++ prgm21.py
@@ -0,0 +1,6 @@
+def modifyList(lst):
+    lst.append(10)
+    print("inside function:",lst)
+number=[1,2,3]
+modifyList(numbers)
+print("outside function:",numbers)
---
+++ prgm22.py
@@ -0,0 +1,7 @@
+def modifyValue(a):
+    print("value recived in function:",a)
+    a=10
+    print("value inside after function change:",a)
+a=5
+modifyValue(a)
+print("value outside function:",a)
