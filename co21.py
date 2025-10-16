4c119ee05916cd51c5f9247999d5fad2471a7d50
shanu3785 <bumblebe919@gmail.com>
2025-10-16 10:35:09 +0530

    Add files via upload

---
+++ co21.py
@@ -0,0 +1,14 @@
+def factorial(n):
+    if n==0:
+        return 1
+    else:
+        return n *factorial(n-1)
+num=int(input("ente a number:"))
+if num<0:
+    print("factorial is not defined for negative numbers:")
+elif num==0:
+    print(f"{num}!=1")
+else:
+    fact = factorial(num)
+    print(f"{num}!={fact}")
+    
